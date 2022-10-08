548cf2d2c5aa70f52391ae3b7cc318c3756f1abc
Maha Javed <59806957+Maha986@users.noreply.github.com>
2022-10-08 20:16:23 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/doctor?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI ="mongodb+srv://pcsir:pcsir1980@clusterdr.fbyyijw.mongodb.net/?retryWrites=true&w=majority";
+ //"mongodb://localhost:27017/doctor?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 const mongodbConnect=()=>{
     mongoose.connect(mongoURI,()=>{
